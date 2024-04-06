e5f8b6d1305176bf75c9dccc9a99d52b1bf970a5
Bananadev3 <angelomodanez30@gmail.com>
2024-04-05 23:54:56 -0300

    Versão2

--- src/app/tabela/tabela.component.ts
+++ src/app/tabela/tabela.component.ts
@@ -10,14 +10,27 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class TabelaComponent {
   private modalService = inject(NgbModal);
 
+  a: string = '';
+  b: string = '';
+  r1: boolean = false;
+  r4: boolean = false;
+  r3: boolean = false;
+  r2: boolean = false;
+
   open(content: TemplateRef<any>) {
-    if (this.p === '' || this.q === '') {
+    if (this.a === '' || this.b === '') {
       alert('Ensira os Valores');
     } else {
       this.modalService.open(content, { backdropClass: 'light-blue-backdrop' });
     }
-  }
 
-  p: string = '';
-  q: string = '';
+    if (this.a === this.b) {
+      this.r1 = true;
+      this.r2 = true;
+      this.r3 = true;
+      this.r4 = true;
+    } else {
+      this.r1 = true;
+    }
+  }
 }
